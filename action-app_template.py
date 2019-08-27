967c96292014fde2ff738e550388929957d18215
Darrell Aucoin <daucoin@uwaterloo.ca>
2019-08-27 11:46:15 -0400

    experimenting

--- action-app_template.py
+++ action-app_template.py
@@ -1,93 +1,30 @@
 # !/usr/bin/env python3
 # encoding: utf-8
 
-# import paho.mqtt.client as mqtt
-
-
-# def on_connect(client, userdata, flags, rc):
-#     print('Connected')
-#     mqtt.subscribe('hermes/intent/#')
-#
-# mqtt = mqtt.Client()
-# mqtt.on_connect = on_connect
-# mqtt.connect('raspberrypi.local', 1883)
-# mqtt.loop_forever()
+#from snipsTools import SnipsConfigParser
 
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
-import pygame
-from pygame.locals import *
-from pygame.math import Vector2
 import sqlite3 as sql
 import os
-import time
-import sys
-# import pygameMenu
 import subprocess
-from sys import platform
-
-if platform == "darwin":
-    # OS X
-    ROOT_DIR = "Users/daucoin/github/PAT_on_Pi"
-    bashStartSnips = ["brew services start mosquitto",
-                      "brew services start snips-audio-server",
-                      "brew services start snips-hotword",
-                      "brew services start snips-tts",
-                      "brew services start snips-nlu",
-                      "brew services start snips-asr",
-                      "brew services start snips-dialogue"]
-    bashStopSnips = ["brew services stop mosquitto",
-                     "brew services stop snips-audio-server",
-                     "brew services stop snips-hotword",
-                     "brew services stop snips-tts",
-                     "brew services stop snips-nlu",
-                     "brew services start snips-asr",
-                     "brew services stop snips-dialogue"]
-
-
-    def start_snips():
-        for command in bashStartSnips:
-            process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-            output, error = process.communicate()
-
-
-    def stop_snips():
-        for command in bashStopSnips:
-            process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-            output, error = process.communicate()
-
-else:
-    # Raspberry pi
-    ROOT_DIR = "/home/pi/PAT"
-
-
-    def start_snips():
-        pass
-
-
-    def stop_snips():
-        pass
-
-start_snips()
+import pygame
+import time
 
 CONFIG_INI = "config.ini"
-
+ROOT_DIR = "/home/pi/PAT"
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
-MQTT_ADDR = f"{MQTT_IP_ADDR}:{str(MQTT_PORT)}"
-
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
-
 screen_size = (1024, 600)
-
+DEBUG = True
 
 def play_mp3(path):
-    # subprocess.Popen(['mplayer', '-nolirc', '-really-quiet', path]).wait()
     subprocess.Popen(['mpg123', '-q', path]).wait()
-    # subprocess.Popen(['mpg321', '-q', path]).wait()
 
 
 class Background(pygame.sprite.Sprite):
@@ -96,26 +33,6 @@ class Background(pygame.sprite.Sprite):
         self.image = pygame.image.load(image_file)
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
-
-
-class ChatBubble(pygame.sprite.Sprite):
-    def __init__(self, position=[450, 100], width=10, height=100):
-        pygame.sprite.Sprite.__init__(self)
-        self.screen = ScreenSingleTone()
-        self.line_width = 5
-        self.bubble_black = pygame.draw.rect(self.screen, BLACK, position + [width] + [height])
-        self.bubble = pygame.draw.rect(self.screen, WHITE,
-                                       position + [width + self.line_width] + [height + self.line_width])
-        # self.bubble_triangle = pygame.draw.polygon()
-        self.font = pygame.font.Font('freesansbold.ttf', 32)
-        # self.bubble = pygame.display.set_mode((X, Y))
-
-    def change_text(self, text):
-        pass
-
-    def render(self):
-        pass
-
 
 class ScreenSingleTone(object):
     __instance = None
@@ -127,34 +44,40 @@ class ScreenSingleTone(object):
         pygame.display.set_caption('PAT')
         return ScreenSingleTone.__instance
 
-
 def insert_image(screen, image, img_pos):
     img = pygame.image.load(image)
     screen.blit(img, img_pos)
 
-
 class PAT_simple:
 
     def __init__(self, position):
-        self.screen = ScreenSingleTone()
-        self.gamer_girl = [
-            pygame.image.load(os.path.join(ROOT_DIR, f'PAT/png/frame_{i}_delay-0.2s.png')).convert_alpha() for i in
-            range(14)]
-        self.frames = self.gamer_girl
-        self.position = position
-        self.BG = Background(os.path.join(ROOT_DIR, 'wise_nebula.png'), [0, 0])
-        self.screen.fill(WHITE)
-        self.screen.blit(self.BG.image, self.BG.rect)
-        self.render_frame(0)
+        if not DEBUG:
+            self.screen = ScreenSingleTone()
+            self.gamer_girl = [
+                pygame.image.load(os.path.join(ROOT_DIR, f'PAT/png/frame_{i}_delay-0.2s.png')).convert_alpha() for i in
+                range(14)]
+            self.frames = self.gamer_girl
+            self.position = position
+            self.BG = Background(os.path.join(ROOT_DIR, 'wise_nebula.png'), [0, 0])
+            self.screen.fill(WHITE)
+            self.screen.blit(self.BG.image, self.BG.rect)
+            self.render_frame(0)
+            self.frame_i = 0
+            self.start_time = time.time()
 
     def talk_animation(self, response, intent="explain"):
+        if DEBUG:
+            _, response_mp3, __, ___, ____, _____ = response[0]
+            file = os.path.join(ROOT_DIR, 'intents', intent, response_mp3)
+            play_mp3(file)
+            return None
         pygame.mixer.fadeout(0.25)
-        song_end = pygame.USEREVENT + 1
-        start_time = time.time()
+        self.start_time = time.time()
         i = 0
         for response_text, response_mp3, actions, image, img_x, img_y in response:
+            song_end = pygame.USEREVENT + 1
             running = True
-            file = os.path.join(ROOT_DIR, 'intents', intent, response_mp3)
+            #file = os.path.join(ROOT_DIR, 'intents', intent, response_mp3)
             if image is not None:
                 insert_image(screen=self.screen, image=image, img_pos=(img_x, img_y))
             try:
@@ -166,43 +89,38 @@ class PAT_simple:
             self.screen.fill(WHITE)
             self.screen.blit(self.BG.image, self.BG.rect)
             self.render_frame(i)
+            self.frame_i = 0
             while running:
-                if time.time() - start_time >= 0.2:
-                    start_time = time.time()
-                    i = (i + 1) % len(self.frames)
-                    self.render_frame(i)
-                for event in pygame.event.get():
-                    if time.time() - start_time >= 0.2:
-                        start_time = time.time()
-                        i = (i + 1) % len(self.frames)
-                        self.render_frame(i)
-                    if event.type == song_end:
-                        running = False
-                        break
+                events = pygame.event.get()
+                #for event in pygame.event.get():
+                self._animate()
+                if 0 == len(events):
+                    continue
+                else:
+                    for event in events:
+                        self._animate()
+                        if event.type == song_end:
+                            running = False
+                            break
 
-        self.screen.fill([255, 255, 255])
+        self.screen.fill(WHITE)
         self.screen.blit(self.BG.image, self.BG.rect)
         self.render_frame(0)
+        self.start_time = time.time()
 
     def render_frame(self, i):
         self.screen.blit(self.frames[i], self.position)
         pygame.display.update()
 
-
-class Menu(object):
-    """
-
-
-    """
-
-    def __init__(self):
-        pass
-
-    def show_menu(self):
-        pass
+    def _animate(self):
+        if time.time() - self.start_time >= 0.2:
+            self.start_time = time.time()
+            self.frame_i = (self.frame_i + 1) % len(self.frames)
+            self.render_frame(self.frame_i)
 
 
-class FAQ_PAT(object):
+
+class Template(object):
     """Class used to wrap action code with mqtt connection
 
         Please change the name refering to your application
@@ -210,13 +128,10 @@ class FAQ_PAT(object):
 
     def __init__(self):
         # get the configuration if needed
-        self._running = True
-        self._display_surf = None
         '''
         try:
-            from snipsTools import SnipsConfigParser
             self.config = SnipsConfigParser.read_configuration_file(CONFIG_INI)
-        except:
+        except :
             self.config = None
         '''
         self.config = None
@@ -224,47 +139,17 @@ class FAQ_PAT(object):
         self.cursor = None
         self.PAT = None
         self.PAT_position = (-200, 100)
-        # file = 'PAT/intents/explain/AppHolo_00.mp3'
-        # play_mp3(file)
-
-    def on_init(self):
-        pygame.init()
-        self._display_surf = ScreenSingleTone()
-        pygame.mixer.init()
-        self.PAT = PAT_simple(self.PAT_position)
         self.con = sql.connect(os.path.join(ROOT_DIR, 'PAT_on_pi.db'))
         self.cursor = self.con.cursor()
         self._running = True
+        # start listening to MQTT
+        self.start_blocking()
+        if not DEBUG:
+            pygame.init()
+            self._display_surf = ScreenSingleTone()
+            pygame.mixer.init()
+        self.PAT = PAT_simple(self.PAT_position)
 
-    def on_event(self, event):
-        if event.type == pygame.QUIT:
-            self._running = False
-            sys.exit()
-
-    def on_loop(self):
-        pass
-
-    def on_render(self):
-        pass
-
-    def _on_cleanup(self):
-        self.con.close()
-        pygame.mixer.quit()
-        pygame.quit()
-        stop_snips()
-
-        sys.exit()
-
-    def on_execute(self):
-        self.on_init()
-        while self._running:
-            for event in pygame.event.get():
-                # start listening to MQTT
-                self.start_blocking()
-                self.on_event(event)
-            self.on_loop()
-            self.on_render()
-        self._on_cleanup()
 
     def _get_slots(self, intent_message, slot_names=[]):
         slots = {}
@@ -274,8 +159,6 @@ class FAQ_PAT(object):
             if slot_name not in slots.keys():
                 slots[slot_name] = "default"
         return slots
-
-    # --> Sub callback function, one per intent
 
     def intent_explain(self, intent_message):
         slots = self._get_slots(intent_message, slot_names=["Components"])
@@ -347,22 +230,15 @@ class FAQ_PAT(object):
         self.PAT.talk_animation(response, intent="hello")
 
     def intent_show_menu(self, Hermes, intent_message):
-
+        pass
         '''
         Not sure what to do here
         '''
 
-    def master_intent_callback(self, Hermes, intent_message):
-        '''
-        --> Master callback function, triggered everytime an intent is recognized
 
-        :param Hermes:
-        :param intent_message:
-        :return:
-        '''
+    # --> Master callback function, triggered everytime an intent is recognized
+    def master_intent_callback(self,hermes, intent_message):
         coming_intent = intent_message['intent']['intentName']
-        # terminate the session first if not continue
-        Hermes.publish_end_session(intent_message.session_id, "")
 
         print("will this show up?")
         if coming_intent == 'Explain':
@@ -377,18 +253,15 @@ class FAQ_PAT(object):
             self.intent_bye(intent_message)
         elif coming_intent == 'Show_Menu':
             self.intent_show_menu(Hermes, intent_message)
-
+        # terminate the session first if not continue
+        hermes.publish_end_session(intent_message.session_id, "")
         print(f'[Received] intent: {intent_message.intent.intent_name}')
+        # more callback and if condition goes here...
 
+    # --> Register callback function and start MQTT
     def start_blocking(self):
-        '''
-        --> Register callback function and start MQTT
-        :return:
-        '''
         with Hermes(MQTT_ADDR) as h:
             h.subscribe_intents(self.master_intent_callback).start()
 
-
 if __name__ == "__main__":
-    PAT = FAQ_PAT()
-    PAT.on_execute()
+    Template()
