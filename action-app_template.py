cc40739a7592e37459199da8597a0a5ceeb9cdfd
Darrell Aucoin <daucoin@uwaterloo.ca>
2019-08-30 14:37:37 -0400

    experimenting

--- action-app_template.py
+++ action-app_template.py
@@ -311,8 +311,8 @@ class FAQ_PAT(object):
         elif self.mp3_only or self.mixer_mp3_only:
             try:
                 img = Image.open(image)
-                img.show()
-                # showPIL(img)
+                # img.show()
+                showPIL(img)
             except:
                 print("image file not found:", image)
 
