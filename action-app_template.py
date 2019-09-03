c556c62ef245579cb9d8060514c317f8f4e0e0f6
Darrell Aucoin <daucoin@uwaterloo.ca>
2019-09-03 14:20:29 -0400

    experimenting

--- action-app_template.py
+++ action-app_template.py
@@ -42,7 +42,7 @@ def insert_image(image, delay=7):
     else:
         image = os.path.join(ROOT_DIR, "images", BG_IMAGE)
         args = ['pqiv', '--fullscreen', "--hide-info-box", "--scale-images-up", image]
-    subprocess.Popen(args=args, preexec_fn=['xdotool', 'key', "Escape"])
+    subprocess.Popen(args=args)
     print("image files:", image)
 
 
